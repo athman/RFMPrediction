892984d63b31855185fd4ed54a3cc888100da06c
Athman Gude  CS281-0837/2010 <athmangude@gmail.com>
2014-10-30 15:00:06 +0300

    Finished installing and confiuring Twitter Bootstrap for the UserInterface

---
+++ app/controllers/datasets_controller.rb
@@ -0,0 +1,4 @@
+class DatasetsController < ApplicationController
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+
+  get 'datasets/create'
+
+  root 'pages#home'
+
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  get 'pages/help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
