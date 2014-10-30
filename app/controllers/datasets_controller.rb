96ff27707be08de92fd47c9fe9a5b04dfe51687b
Athman Gude  CS281-0837/2010 <athmangude@gmail.com>
2014-10-30 21:36:37 +0300

    >>Working validations for dataset >>Removing errors in dataset form

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -1,48 +1,57 @@
 class DatasetsController < ApplicationController
 
-  def index
-  end
-
-  def new
-	@dataset = Dataset.new
-  end
-
-  def create
-	@dataset = Dataset.new(dataset_params)
-	if @dataset.valid?
-	  flash[:notice] = "Received valid data"
-	  redirect_to @dataset
-	else
-	  render 'new'
-	end
-  end
-
-  def show
-	@dataset = dataset.find(params[:id])
-  end
-
-  def edit
-  end
-
-  def update
-	@dataset = dataset.find(params[:id])
-	if @dataset.update_attributes(dataset_params)
-	  flash[:success] = "Dataset updated successfully"
-	  redirect_to @dataset
-	else
-	  render "edit"
+	def index
 	end
 
-  end
+	def new
+		@dataset = Dataset.new
+	end
 
-  def destroy
-	Dataset.find(params[:id]).destroy
-	flash[:success] = "The dataset has been deleted successfully"
-	redirect_to datasets_path
-  end
+	def create
+		@dataset = Dataset.new(dataset_params)
+		if @dataset.valid?
 
-  def dataset_params
-	params.require(:dataset).permit(:min_latitude, :max_latitude, :min_longitude, :max_longitude, :min_gain, :max_gain, :min_height, :max_height, :quantity)
-  end
+			generated_set = Array.new
+
+			@dataset.quantity.times do
+				iterating_array = Array.new
+
+			end
+
+			flash[:notice] = "#{@dataset.min_latitude}"
+
+			redirect_to @dataset
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@dataset = dataset.find(params[:id])
+	end
+
+	def edit
+	end
+
+	def update
+		@dataset = dataset.find(params[:id])
+		if @dataset.update_attributes(dataset_params)
+			flash[:success] = "Dataset updated successfully"
+			redirect_to @dataset
+		else
+			render "edit"
+		end
+
+	end
+
+	def destroy
+		Dataset.find(params[:id]).destroy
+		flash[:success] = "The dataset has been deleted successfully"
+		redirect_to datasets_path
+	end
+
+	def dataset_params
+		params.require(:dataset).permit(:min_latitude, :max_latitude, :min_longitude, :max_longitude, :min_gain, :max_gain, :min_height, :max_height, :quantity)
+	end
 
 end
--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -1,4 +1,15 @@
 class Dataset
 	include ActiveModel::Model
+	include ActiveModel::Validations
+
 	attr_accessor :min_latitude, :max_latitude, :min_longitude, :max_longitude, :min_gain, :max_gain, :min_height, :max_height, :quantity
+	validates :min_latitude, presence: true, numericality: true
+	validates :max_latitude, presence: true, numericality: true
+	validates :min_longitude, presence: true, numericality: true
+	validates :max_longitude, presence: true, numericality: true
+	validates :min_gain, presence: true, numericality: true
+	validates :max_gain, presence: true, numericality: true
+	validates :min_height, presence: true, numericality: true
+	validates :max_height, presence: true, numericality: true
+	validates :quantity, presence:true, numericality:true
 end
