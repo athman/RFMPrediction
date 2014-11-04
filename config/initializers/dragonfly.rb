48afc9ba27b951b88906d3585a5813a3d006167e
Athman Gude  CS281-0837/2010 <athmangude@gmail.com>
2014-11-04 11:09:07 +0300

    Configuring Dragonfly

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -9,9 +9,18 @@ Dragonfly.app.configure do
 
   url_format "/media/:job/:name"
 
-  datastore :file,
-    root_path: Rails.root.join('public/system/dragonfly', Rails.env),
-    server_root: Rails.root.join('public')
+
+    if Rails.env.development? || Rails.env.test?
+        datastore :file,
+            root_path: Rails.root.join('public/system/dragonfly', Rails.env),
+            server_root: Rails.root.join('public')
+    else
+        datastore :s3,
+            bucket_name: "rfmprediction-datasets",
+            access_key_id: "AKIAJU6P5VS4ZJYKVS2A",
+            secret_access_key: "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",
+            url_scheme: 'https'
+    end
 end
 
 # Logger
@@ -25,41 +34,3 @@ if defined?(ActiveRecord::Base)
   ActiveRecord::Base.extend Dragonfly::Model
   ActiveRecord::Base.extend Dragonfly::Model::Validations
 end
-
-
-
-
-app = Dragonfly.app
-
-app.configure_with(:imagemagick)
-#app.configure_with(:rails)
-if Rails.env.production?
-  app.configure do |c|
-    c.datastore = Dragonfly::DataStorage::S3DataStore.new(
-      :bucket_name => 'rfmprediction-datasets',
-      :access_key_id => ENV['S3_KEY'],
-      :secret_access_key => ENV['S3_SECRET']
-    )
-  end
-end
-
-app.define_macro(ActiveRecord::Base, :dataset_accessor)
-
-
-
-
-# Configuration for Amazon s3
-# url_format "/media/:job/:name"
-if Rails.env.development? || Rails.env.test?
-    datastore :file,
-        root_path: Rails.root.join('public/system/dragonfly', Rails.env),
-        server_root: Rails.root.join('public')
-end
-
-#else
-#    datastore :s3,
-#        bucket_name: "rfmprediction-datasets",
-#        access_key_id: "AKIAJU6P5VS4ZJYKVS2A",
-#        secret_access_key: "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",
-#        url_scheme: 'https'
-#end
