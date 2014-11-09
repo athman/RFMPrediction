efbdba3c54c560f4c5111b22c1e79b66e476465a
Athman Gude  CS281-0837/2010 <athmangude@gmail.com>
2014-11-09 17:48:57 +0300

    >>Fixed issue with changing locations of markers on page reloads >>Determining meters that are unlikely to communicate

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -110,6 +110,7 @@ For each meter
             meter_result['height'] = meter['height']
             meter_result['gain'] = meter['gain']
             meter_result['agents_in_vicinity'] = Array.new
+            meter_result['agents_can_communicate'] = Array.new
 
             receiving_agent.agents.each do |transmitting_meter|
                 if receiving_agent.in_vicinity?(transmitting_meter) && !receiving_agent.is_self?(transmitting_meter)
@@ -117,7 +118,6 @@ For each meter
                     agent_in_vicinity['agent'] = transmitting_meter
                     distance = receiving_agent.distance_between_myself_and_agent transmitting_meter
                     agent_in_vicinity['distance'] = distance
-                    #@distances.push distance
                     friis_power_received = receiving_agent.friis_power_received transmitting_meter
                     agent_in_vicinity['friis_power_received'] = friis_power_received
                     signal_lost = receiving_agent.signal_lost transmitting_meter
@@ -130,87 +130,59 @@ For each meter
             end
 
             meter_result['resultant_theoretical_power_received'] = receiving_agent.resultant_theoretical_power_received theoretical_power_received_set
-            meter_result['agents-communicating-count'] = meter_result['agents_in_vicinity'].count
+            meter_result['agents_in_vicinity_count'] = meter_result['agents_in_vicinity'].count
 
             if meter_result['agents_in_vicinity'].any?
-                meter_result['can-communicate'] = true
-            else
-                meter_result['can-communicate'] = false
+                meter_result['agents_in_vicinity'].each do |agent|
+                    if agent['theoretical_power'].to_f > -85
+                        meter_result['agents_can_communicate'].push agent
+                    end
+                end
             end
 
+            meter_result['agents_can_communicate_count'] = meter_result['agents_can_communicate'].count
+
+            if meter_result['agents_can_communicate_count'] > 0
+                meter_result['can_communicate'] = true
+            else
+                meter_result['can_communicate'] = false
+            end
 
             #meter_result['antenna_type'] = receiving_agent.antenna_type
             @results_set.push meter_result
 
         end
 
-
-        #render xml: @meters
-        #render xml: @meters
-
-        #@inspect = @results_set
-        #render xml: @results_set
-
-        @markers = Array.new
-
-#        @results_set.each do |result|
-#            marker = Hash.new
-#            marker['lat'] = result['latitude']
-#            marker['lng'] = result['longitude']
-#            marker['title'] = "A meter"
-#            marker['picture'] = {
-#				url: "https://s3.amazonaws.com/rfmprediction-assets/2014/11/07/14/27/36/278/antenna_36x36.png",
-#				#url: "http://simpleicon.com/wp-content/uploads/antenna-3.png",
-#				#url: "https://addons.cdn.mozilla.net/img/uploads/addon_icons/13/13028-64.png",
-#				#url: "/app/assets/images/antenna.png",
-#				width:  36,
-#				height: 36
-#			  }
-#            marker['infowindow'] = "A meter description"
-#            @markers.push marker
-#        end
-
         @markers = Gmaps4rails.build_markers(@results_set) do |meter, marker|
             marker.lat meter['latitude']
             marker.lng meter['longitude']
-            marker.title meter['height']
-            marker.infowindow meter['gain']
-            marker.picture({
-                    :url => "https://s3.amazonaws.com/rfmprediction-assets/2014/11/07/14/27/36/278/antenna_36x36.png",
-                    #:picture => "http://www.blankdots.com/img/github-32x32.png",
-                    #:picture => "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=A|FF0000|000000",
-                    :width => 36,
-                    :height => 36
+            marker.title "Latitude[#{meter['latitude']}]  Longitude[#{meter['longitude']}]"
+            marker.infowindow "Latitude[ #{meter['latitude']} ]  Longitude[ #{meter['longitude']} ]"
+
+            if meter['can_communicate'] == true
+                marker.picture({
+                    url: "https://s3.amazonaws.com/rfmprediction-assets/2014/11/07/14/27/36/278/antenna_36x36.png",
+                    width: 36,
+                    height: 36,
+                    anchor: [40, 58]
                 })
-
-
-            #if meter['can-communicate'] = true
-            #    marker.picture({
-            #        :picture => "https://s3.amazonaws.com/rfmprediction-assets/2014/11/07/14/27/36/278/antenna.png",
-            #        #:picture => "http://www.blankdots.com/img/github-32x32.png",
-            #        #:picture => "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=A|FF0000|000000",
-            #        :width => 32,
-            #        :height => 32
-            #    })
-            #end
+            else
+                marker.picture({
+                    url: "https://s3.amazonaws.com/rfmprediction-assets/2014/11/07/14/27/36/278/antenna_36x36_red.png",
+                    width: 36,
+                    height: 36,
+                    anchor: [40, 58]
+                })
+            end
         end
 
         render "map"
         #render json: @markers
-    end
 
-    #def gmaps4rails_marker_picture
-    #    {
-    #        "picture" => "https://s3.amazonaws.com/rfmprediction-assets/2014/11/07/14/27/36/278/antenna.png",
-    #        "width" => 20,
-    #        "height" => 20,
-    #        "marker_anchor" => [ 5, 10],
-    #        #"shadow_picture" => "/images/morgan.png" ,
-    #        "shadow_width" => "110",
-    #        "shadow_height" => "110",
-    #        "shadow_anchor" => [5, 10],
-    #    }
-    #end
+        #@inspect = @results_set
+        #render xml: @results_set
+        #render "inspect"
+    end
 
     private
       # Use callbacks to share common setup or constraints between actions.
